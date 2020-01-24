f1eac4b9ac9a00d7a02855e5e5a4772ad3dc0018
RiskTolerance <isaac.druin@gmail.com>
2020-01-24 11:11:40 -0800

    working through the second iteration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,7 +2,27 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array) {
+    let newArray = array.slice();
+    newArray.sort(function(a, b) {
+        if (a.year < b.year) {return -1;}
+        if (a.year > b.year) {return 1;}
+        if (a.title < b.title) {return -1;}
+        if (a.title > b.title) {return 1;}
+    })
+    return newArray;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+function howManyMovies (array) {
+    if (array == []) {
+        return 0;
+    }
+    else {
+        array.forEach(obj => console.log('yes',obj,obj.title));
+    }
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
